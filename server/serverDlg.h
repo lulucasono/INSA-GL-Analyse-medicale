7a064d7b3a636c903ae26f941620b49e48420905
Marc-Antoine FERNANDES <embraser01@gmail.com>
2017-04-10 17:34:31 +0200

    Add MFC server

---
+++ server/serverDlg.h
@@ -0,0 +1,34 @@
+
+// serverDlg.h : fichier d'en-tête
+//
+
+#pragma once
+
+
+// boîte de dialogue CserverDlg
+class CserverDlg : public CDialogEx
+{
+// Construction
+public:
+	CserverDlg(CWnd* pParent = NULL);	// constructeur standard
+
+// Données de boîte de dialogue
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_SERVER_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// Prise en charge de DDX/DDV
+
+
+// Implémentation
+protected:
+	HICON m_hIcon;
+
+	// Fonctions générées de la table des messages
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};

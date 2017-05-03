e84e9f28210f695c75bfd7775203f36c6f709a6a
ye_yuan <wsr10000@hotmail.com>
2017-05-03 16:53:20 +0200

    disease added

--- server/Disease.cpp
+++ server/Disease.cpp
@@ -1,10 +1,23 @@
 #include "stdafx.h"
 #include "Disease.h"
+#include <iostream>
+using namespace std;
 
 
-Disease::Disease()
+Disease::Disease(string name,list<string>li):d_name(name),d_list(li)
 {
 }
+
+const string& Disease::name() const
+{
+	return d_name;
+}
+
+const list<string>& Disease::riskfulGenes() const
+{
+	return d_list;
+}
+
 
 
 Disease::~Disease()
--- server/Disease.h
+++ server/Disease.h
@@ -1,8 +1,24 @@
 #pragma once
+#include <iostream>
+#include <list>
+using namespace std;
+
 class Disease
 {
 public:
-	Disease();
+	//////constructor///////
+	Disease(string cs,list<string> li);
+
+	//////getter and setter//////
+
+	const string& name() const;
+	const list<string>& riskfulGenes() const;
+
+	//////destructor//////
 	virtual ~Disease();
+private:
+	string d_name;
+	list<string> d_list;
+
 };
 

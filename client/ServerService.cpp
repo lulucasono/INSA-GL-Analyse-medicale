22f5458cf998c55e23bfe8929b52d04dd3a71539
DESKTOP-CIJ17O9\julia <julialudac@yahoo.fr>
2017-05-18 23:41:04 +0200

    - pour côté client : ServerService : Coquilles/corps des méthodes + quelques codes - add schéma fonctionnement Socket

--- client/ServerService.cpp
+++ client/ServerService.cpp
@@ -1,12 +1,65 @@
 #include "stdafx.h"
 #include "ServerService.h"
+#include <iostream>
 
+using namespace std;
+
+//typedef std::pair<std::string, std::string> pairServer;
+
+
+void ServerService::updateFromMulticast()
+{
+	// Récupération des serveurs par multicast, en écoutant pendant 10 secondes + Exception eventuelle
+	std::map<std::string, std::string> foundServers;
+	// CODE TODO (Julia)  : voir Slack
+
+	// Lecture des serveurs déjà dans la BD + Exception eventuelle
+	std::map<std::string, std::string> fileServers;
+	// try ... 
+	fileServers = getServers();
+	// ... catch 
+
+	// Les serveurs qui ne sont pas déjà dans la BD et qui sont récupérés sont ajoutés à la base 
+	// + Exception eventuelle
+	for(std::map<std::string, std::string>::iterator it = foundServers.begin(); it!= foundServers.end(); it++)
+	{
+		if(fileServers.find(it->first)==fileServers.end())
+		{
+			// Insertion de *it dans la BD + Exception eventuelle (impossible d'insérer machin)
+			Server s(it->first, it->second);
+			addServer(s);
+		}
+	}
+	
+}
+
+void ServerService::addServer(Server s)
+{
+	// écriture dans la BD + Exception eventuelle
+	//  = appel du DAO = TODO
+}
+
+void ServerService::removeServer(Server s)
+{
+	// écriture dans la BD + Exception eventuelle
+	//  = appel du DAO = TODO
+}
+
+std::map<std::string, std::string> ServerService::getServers()
+// la base de données choisie est SQLite
+{
+	// lecture des serveurs stockés depuis la BD
+	//  = appel du DAO = TODO
+
+}
 
 ServerService::ServerService()
 {
+
 }
 
 
 ServerService::~ServerService()
 {
+
 }
--- client/ServerService.h
+++ client/ServerService.h
@@ -1,8 +1,32 @@
 #pragma once
+#pragma once
+#include <map>
+#include "Server.h"
+
+
+
 class ServerService
 {
 public:
+	void updateFromMulticast();
+	// Récupère tous les serveurs détectés par multicast, en écoutant pendant 10 secondes, puis les ajoute 
+	// éventuellement à la base de données
+
+	void addServer(Server s);
+	// Ajoute le serveur s à la base de données 
+	// C'est une méthode manuelle, que l'user peut utiliser,
+	// mais aussi utilisée par la fonctionnalité de mise à jour du serveur
+
+	void removeServer(Server s);
+	// C'est une méthode manuelle, que l'user peut utiliser
+
+	std::map<std::string, std::string> getServers();
+	// Retourne les serveurs stockés dans la base de données 
+	// Dans un stockage, un serveur est représenté par une paire de strings : 
+	// son adresse IP(unique) et son nom 
+
 	ServerService();
 	~ServerService();
+
 };
 

f3542a360463f7b1b074be7062e1c9168a8503da
wth <tianhao.wu@insa-lyon.fr>
2017-05-19 15:28:35 +0200

    ajout de deseaseDAO

--- server/diseaseDAO.cpp
+++ server/diseaseDAO.cpp
@@ -1,12 +1,22 @@
 #include "stdafx.h"
 #include "diseaseDAO.h"
-
+#include "Disease.h"
 
 diseaseDAO::diseaseDAO()
 {
 }
 
+diseaseDAO::diseaseDAO(string dbPath)
+{
+	//sqlite3_open(dbPath,&db);
+
+}
 
 diseaseDAO::~diseaseDAO()
 {
+}
+
+list<Disease> diseaseDAO::findAll()
+{
+	return list<Disease>();
 }

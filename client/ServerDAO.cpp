0e4ae4aa0c2e662af4916e1ab50eaca09dd10b73
wth <tianhao.wu@insa-lyon.fr>
2017-05-19 17:03:44 +0200

    serverDao fini

--- client/ServerDAO.cpp
+++ client/ServerDAO.cpp
@@ -12,27 +12,55 @@ ServerDAO::ServerDAO(const char * dbName)
 
 ServerDAO::~ServerDAO()
 {
+
 }
 
 list<Server> ServerDAO::findAll()
 {
 	string sqlSelectStr = "SELECT * FROM Server;";
 	const char *sqlSelect = sqlSelectStr.c_str();
-	return list<Server>();
-}
-
-Server ServerDAO::findByName(string nameServer) 
-{
+	list<Server> servers;
 	int code;
-	string sqlSelectStr = "SELECT * FROM Server WHERE name='"+nameServer+"';";
-	const char *sqlSelect = sqlSelectStr.c_str();
 	char **results = NULL;
 	int rows, columns;
 	char *error;
 
 	code = sqlite3_get_table(db, sqlSelect, &results, &rows, &columns, &error);
 
-	Server server();
+	if (code != 0)
+	{
+		cerr << "Error executing SQLite3 query (read_patient): " << sqlite3_errmsg(db) << endl;
+		sqlite3_free(error);
+	}
+	else
+	{
+		for (int i = 0; i < rows; i++)
+		{
+			string adresse = results[i + columns];
+			string * p_adresse = &adresse;
+			string nom = results[i+1 + columns];
+			Server server;
+			server.SetAddress(p_adresse);
+			server.SetName(nom);
+			servers.push_back(server);
+		}
+	}
+
+	return servers;
+}
+
+Server ServerDAO::findByName(string nameServer) 
+{
+	string sqlSelectStr = "SELECT * FROM Server WHERE name='"+nameServer+"';";
+	const char *sqlSelect = sqlSelectStr.c_str();
+	int code;
+	char **results = NULL;
+	int rows, columns;
+	char *error;
+
+	code = sqlite3_get_table(db, sqlSelect, &results, &rows, &columns, &error);
+
+	Server server;
 
 	if (code != 0)
 	{
@@ -44,7 +72,8 @@ Server ServerDAO::findByName(string nameServer)
 		string adresse = results[0 + columns];
 		string * p_adresse = &adresse;
 		string nom = results[1 + columns];
-		//TODO set le attributs de server
+		server.SetAddress(p_adresse);
+		server.SetName(nom);
 	}
 
 	return server;

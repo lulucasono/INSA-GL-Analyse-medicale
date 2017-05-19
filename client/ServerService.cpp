ab50c3068fd7c28b73e309fa8527b859dd488ba8
DESKTOP-CIJ17O9\julia <julialudac@yahoo.fr>
2017-05-19 15:07:48 +0200

    Correction map Servers

--- client/ServerService.cpp
+++ client/ServerService.cpp
@@ -4,29 +4,26 @@
 
 using namespace std;
 
-//typedef std::pair<std::string, std::string> pairServer;
-
-
 void ServerService::UpdateFromMulticast()
 {
 	// Récupération des serveurs par multicast, en écoutant pendant 10 secondes + Exception eventuelle
-	std::map<std::string, std::string> foundServers;
+	std::map<std::string, Server> foundServers;
 	// CODE TODO (Julia)  : voir Slack 
 
 	// Lecture des serveurs déjà dans la BD + Exception eventuelle
-	std::map<std::string, std::string> fileServers;
+	std::map<std::string, Server> fileServers;
 	// try ... 
 	fileServers = GetServers();
 	// ... catch 
 
 	// Les serveurs qui ne sont pas déjà dans la BD et qui sont récupérés sont ajoutés à la base 
 	// + Exception eventuelle
-	for(std::map<std::string, std::string>::iterator it = foundServers.begin(); it!= foundServers.end(); it++)
+	for(std::map<std::string, Server>::iterator it = foundServers.begin(); it!= foundServers.end(); it++)
 	{
 		if(fileServers.find(it->first)==fileServers.end())
 		{
 			// Insertion de *it dans la BD + Exception eventuelle (impossible d'insérer machin)
-			Server s(it->first, it->second);
+			Server s(it->first, it->second.name());
 			AddServer(s);
 		}
 	}
@@ -45,10 +42,10 @@ void ServerService::RemoveServer(Server s)
 	//  = appel du DAO = TODO
 }
 
-std::map<std::string, std::string> ServerService::GetServers()
+std::map<std::string, Server> ServerService::GetServers()
 // la base de données choisie est SQLite
 {
-	std::map<std::string, std::string> servers;
+	std::map<std::string, Server> servers;
 	// lecture des serveurs stockés depuis la BD
 	//  = appel du DAO = TODO
 	// try ...

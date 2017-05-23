728b2bb041185587cc1c107e2a3c8d75df760fc8
lulucasono <lulucasono@gmail.com>
2017-05-23 11:45:56 +0200

    Added genome models to the server

--- MedAnalysisServer/models/Genome.h
+++ MedAnalysisServer/models/Genome.h
@@ -1,4 +1,19 @@
 #ifndef MEDANALYSIS_SERVER_GENOME_H
 #define MEDANALYSIS_SERVER_GENOME_H
+#pragma once
+#include <list>
+
+using namespace std;
+
+class Genome
+{
+public:
+    Genome();
+    virtual ~Genome();
+    const list<string>& getGene() const;
+private:
+    list<string> geneticCode;
+};
+
 
 #endif //MEDANALYSIS_SERVER_GENOME_H

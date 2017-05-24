36be03e067fdc32bee9f1b09a290fee7f64e9926
Marc-Antoine FERNANDES <embraser01@gmail.com>
2017-05-24 09:16:29 +0200

    Add error response

--- MedAnalysisServer/utils/utils.cpp
+++ MedAnalysisServer/utils/utils.cpp
@@ -2,24 +2,33 @@
 
 template<typename Out>
 void split(const std::string &s, char delim, Out result, bool emptyString) {
-  std::stringstream ss;
-  ss.str(s);
-  std::string item;
-  if (emptyString) {
-    while (std::getline(ss, item, delim)) {
-      *(result++) = item;
+    std::stringstream ss;
+    ss.str(s);
+    std::string item;
+    if (emptyString) {
+        while (std::getline(ss, item, delim)) {
+            *(result++) = item;
+        }
+    } else {
+        while (std::getline(ss, item, delim)) {
+            if (!item.empty()) {
+                *(result++) = item;
+            }
+        }
     }
-  } else {
-    while (std::getline(ss, item, delim)) {
-      if (!item.empty()) {
-        *(result++) = item;
-      }
-    }
-  }
 }
 
-std::vector <std::string> split(const std::string &s, char delim, bool emptyString) {
-  std::vector <std::string> elems;
-  split(s, delim, std::back_inserter(elems), emptyString);
-  return elems;
+std::vector<std::string> split(const std::string &s, char delim, bool emptyString) {
+    std::vector<std::string> elems;
+    split(s, delim, std::back_inserter(elems), emptyString);
+    return elems;
+}
+
+std::vector<std::string> getError(std::string errorMsg) {
+
+    std::vector<std::string> res;
+
+    res.push_back("MA v1.0");
+    res.push_back("ERROR " + errorMsg);
+    return res;
 }

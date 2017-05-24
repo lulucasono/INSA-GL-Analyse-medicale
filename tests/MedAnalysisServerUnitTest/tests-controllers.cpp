3a7f45c1abc491b3ab5fa0f7561325a48bbc9e1c
lulucasono <lulucasono@gmail.com>
2017-05-24 15:01:46 +0200

    FIxed input format

--- tests/MedAnalysisServerUnitTest/tests-controllers.cpp
+++ tests/MedAnalysisServerUnitTest/tests-controllers.cpp
@@ -1,1 +1,23 @@
 #include <catch.hpp>
+#include <controllers/ListController.h>
+#include <controllers/OneController.h>
+
+using Catch::Matchers::Vector::ContainsElementMatcher;
+using std::string;
+using std::vector;
+
+TEST_CASE("One Controller","[controller][server]"){
+    vector<string> str;
+    vector<string>input;
+    input.push_back("MA v1.0");
+    input.push_back("CHECK DISEASES");
+    input.push_back("Maladie");
+    input.push_back("AGGT;CGT");
+    input.push_back("");
+    str=OneController().doOneEvaluation(input,"../../../res/diseases.txt");
+    REQUIRE(str[0]=="MA v1.0");
+    REQUIRE(str[1]=="DISEASE Maladie");
+    //REQUIRE(str[2]=="");
+    REQUIRE(str[2]=="1");
+    REQUIRE(str[3]=="");
+}

d645315572fa048d207ba7b38d856739f2cd4716
wth <tianhao.wu@insa-lyon.fr>
2017-05-24 10:18:16 +0200

    added error de AllController

--- MedAnalysisServer/controllers/AllController.cpp
+++ MedAnalysisServer/controllers/AllController.cpp
@@ -1,6 +1,7 @@
 #include "AllController.h"
 
 const static char EOP = ';';
+const static char *ALLOWED_CHAR = "ATCG";
 
 vector<string> AllController::doCompleteEvaluation(vector<string> req) {
     // TODO Parsing de la requête
@@ -9,24 +10,21 @@ vector<string> AllController::doCompleteEvaluation(vector<string> req) {
     AnalysisService service;
     string word_list = req[2];
 
+    vector<string> res;
     vector<string> genes = split(word_list,EOP,true);
     int i = 0;
 
-    for (i=0;i<genes.size();i++)
+    if(check_char_presence(genes,ALLOWED_CHAR))
     {
-        chech_char_presence()
+        return getError("Unauthorized character found in the genetic code, aborting analysis");
     }
-    Genome genome = Genome(genes);
-
-    vector<Disease> diseases = service.evaluateAll(genome);
-
-    vector<string> res;
     res.push_back("MA v1.0");
+    Genome genome = Genome(genes);
+    vector<Disease> diseases = service.evaluateAll(genome);
     for (i = 0;i<diseases.size();i++)
     {
         res.push_back("DISEASE "+diseases[i].name());
     }
-    res.push_back("");
     res.push_back("");
 
     return res;

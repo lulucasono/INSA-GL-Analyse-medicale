f17498e3ea8d43feb179449c5978324e3d451129
wth <tianhao.wu@insa-lyon.fr>
2017-05-24 09:40:13 +0200

    allController

--- MedAnalysisServer/controllers/AllController.cpp
+++ MedAnalysisServer/controllers/AllController.cpp
@@ -1,8 +1,18 @@
 #include "AllController.h"
 
+const static char EOP = ';';
+
 vector<string> AllController::doCompleteEvaluation(vector<string> req) {
     // TODO Parsing de la requête
     // TODO Check génome format
 
-    return vector<string>();
+    AnalysisService service;
+    string word_list = req[2];
+
+    vector<string> genes = split(word_list,EOP,true);
+
+    vector<string> res;
+    res.push_back("MA v1.0");
+
+    return res;
 }

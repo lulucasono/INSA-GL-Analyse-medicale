0ef5a7014235a25febcb7fa5d5a1cfd29dc75240
lulucasono <lulucasono@gmail.com>
2017-05-26 00:49:41 +0200

    copy from string utils on server

--- MedAnalysisClient/storage/GenomeDAO.h
+++ MedAnalysisClient/storage/GenomeDAO.h
@@ -5,6 +5,16 @@
 #ifndef MEDANALYSIS_GENOMEDAO_H
 #define MEDANALYSIS_GENOMEDAO_H
 
+#include <string>
+#include <vector>
+#include <fstream>
+#include "../exceptions/ReadException.h"
+#include "../models/Genome.h"
+#include "../utils/utils.h"
+
+using std::string;
+using std::vector;
+using std::ifstream;
 
 class GenomeDAO {
 public:
@@ -17,6 +27,7 @@ public:
 
 private:
     Genome genome;
+    string name;
 };
 
 

ca0f7d924c14c4c1dd83f6fbfd58c66b864e0e9c
lulucasono <lulucasono@gmail.com>
2017-05-26 10:26:54 +0200

    Tests for n and rn

--- tests/MedAnalysisServerUnitTest/tests-storage.cpp
+++ tests/MedAnalysisServerUnitTest/tests-storage.cpp
@@ -24,11 +24,13 @@ TEST_CASE( "1.2 finding diseases by name", "[storage][server]" ) {
     REQUIRE(diseasePair.first->second.riskfulGenes()[1]=="CGT");
 }
 
-TEST_CASE( "1.3 loading diseases names on CRLF file", "[storage][server]" ) {
-    DiseaseDAO diseaseDAO = DiseaseDAO("../../../res/Dictionary.txt");
+TEST_CASE( "1.3 loading diseases names on LF file", "[storage][server]" ) {
+    DiseaseDAO diseaseDAO = DiseaseDAO("../../../res/DictionaryLF.txt");
     auto diseasesMap = diseaseDAO.findAll();
     std::vector<std::string> names;
-    names.push_back("Maladie");
+    names.push_back("Hemochromatosis");
+    names.push_back("Asthma");
+    names.push_back("Asthma");
     int i = 0;
     unordered_multimap< std::string, Disease>::iterator iter;
     for( iter=diseasesMap.begin();     iter!=diseasesMap.end();   iter++)
@@ -38,7 +40,34 @@ TEST_CASE( "1.3 loading diseases names on CRLF file", "[storage][server]" ) {
     }
 }
 
-TEST_CASE( "1.4 finding diseases by name on CRLF file", "[storage][server]" ) {
+TEST_CASE( "1.4 finding diseases by name on LF file", "[storage][server]" ) {
+    DiseaseDAO diseaseDAO = DiseaseDAO("../../../res/DictionaryLF.txt");
+    auto diseasePair = diseaseDAO.findByName("Hemochromatosis");
+    REQUIRE(diseasePair.first->first=="Hemochromatosis");
+    REQUIRE(diseasePair.first->second.name()=="Hemochromatosis");
+    REQUIRE(diseasePair.first->second.riskfulGenes()[0]=="TATT");
+    REQUIRE(diseasePair.first->second.riskfulGenes()[1]=="TGCATG");
+    REQUIRE(diseasePair.first->second.riskfulGenes()[2]=="CTTT");
+    REQUIRE(diseasePair.first->second.riskfulGenes()[3]=="TGAA");
+}
+
+TEST_CASE( "1.5 loading diseases names on CRLF file", "[storage][server]" ) {
+    DiseaseDAO diseaseDAO = DiseaseDAO("../../../res/Dictionary.txt");
+    auto diseasesMap = diseaseDAO.findAll();
+    std::vector<std::string> names;
+    names.push_back("Marfan Syndrome");
+    names.push_back("Huntington's Disease");
+    names.push_back("Asthma");
+    int i = 0;
+    unordered_multimap< std::string, Disease>::iterator iter;
+    for( iter=diseasesMap.begin();     iter!=diseasesMap.end();   iter++)
+    {
+        REQUIRE(iter->first ==names[i]);
+        i++;
+    }
+}
+
+TEST_CASE( "1.6 finding diseases by name on CRLF file", "[storage][server]" ) {
     DiseaseDAO diseaseDAO = DiseaseDAO("../../../res/Dictionary.txt");
     auto diseasePair = diseaseDAO.findByName("Maladie");
     REQUIRE(diseasePair.first->first=="Maladie");
